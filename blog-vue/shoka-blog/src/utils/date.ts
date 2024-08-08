d6a55de1a588613e605bd51e42b3e1696e0980be
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-08-08 11:47:07 +0800

    Delete blog-vue directory

--- blog-vue/shoka-blog/src/utils/date.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs";
-
-export function formatDate(date: string | Date, format = "YYYY-MM-DD") {
-  return dayjs(date).format(format);
-}
-
-export function formatDateTime(date: string | Date, format = "YYYY-MM-DD HH:mm:ss") {
-  return formatDate(date, format);
-}
