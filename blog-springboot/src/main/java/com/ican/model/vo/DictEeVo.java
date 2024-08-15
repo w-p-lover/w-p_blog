2bce4381abcbd2cd41ecc65690f8efc7a30f602f
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-08-15 11:12:06 +0800

    设置Excel模版类属性 修改用户token过期时间

--- blog-springboot/src/main/java/com/ican/model/vo/DictEeVo.java
+++ blog-springboot/src/main/java/com/ican/model/vo/DictEeVo.java
@@ -1,9 +1,16 @@
 package com.ican.model.vo;
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.alibaba.excel.annotation.write.style.ContentRowHeight;
+import com.alibaba.excel.annotation.write.style.ContentStyle;
+import com.alibaba.excel.annotation.write.style.HeadRowHeight;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 
 import javax.validation.constraints.NotBlank;
 import java.util.List;
@@ -16,6 +23,10 @@ import java.util.List;
  * @Filename：DictEeVo
  */
 @Data
+@HeadRowHeight(25)
+@ContentRowHeight(26)
+@EqualsAndHashCode
+@ColumnWidth(15)
 @ApiModel(description = "文章导出")
 public class DictEeVo {
     /**
@@ -26,34 +37,57 @@ public class DictEeVo {
     private Integer id;
 
     /**
-     * 文章缩略图
-     */
-    @ExcelProperty(value = "文章缩略图",index = 1)
-    @ApiModelProperty(value = "文章缩略图")
-    private String articleCover;
-
-    /**
      * 文章标题
      */
-    @ExcelProperty(value = "文章标题",index = 2)
+    @ExcelProperty(value = "文章标题",index = 1)
     @ApiModelProperty(value = "文章标题")
     private String articleTitle;
 
     /**
      * 文章内容
      */
-    @ExcelProperty(value = "文章内容",index = 3)
+    @ExcelProperty(value = "文章内容",index = 2)
     @ApiModelProperty(value = "文章内容")
+    @ColumnWidth(60) // 设置列宽（可以根据需要调整宽度）
     private String articleContent;
 
     /**
      * 文章类型 (1原创 2转载 3翻译)
      */
-    @ExcelProperty(value = "文章类型",index = 4)
+    @ExcelProperty(value = "文章类型",index = 3)
     @ApiModelProperty(value = "文章类型 (1原创 2转载 3翻译)")
     private String articleType;
 
-/*    *//**
+    /**
+     * 是否置顶 (0否 1是)
+     */
+    @ExcelProperty(value = "是否置顶",index = 4)
+    @ApiModelProperty(value = "是否置顶")
+    private String isTop;
+
+    /**
+     * 是否推荐 (0否 1是)
+     */
+    @ExcelProperty(value = "文章推荐",index = 5)
+    @ApiModelProperty(value = "是否推荐 (0否 1是)")
+    private String isRecommend;
+
+    /**
+     * 状态 (1公开 2私密 3草稿)
+     */
+    @ExcelProperty(value = "文章状态",index = 6)
+    @ApiModelProperty(value = "状态 (1公开 2私密 3草稿)")
+    private String status;
+
+    /**
+     * 文章缩略图
+     */
+    @ExcelProperty(value = "文章缩略图",index = 7)
+    @ColumnWidth(90) // 设置列宽（可以根据需要调整宽度）
+    @ApiModelProperty(value = "文章缩略图")
+    private String articleCover;
+
+    /*    *//**
      * 分类名
      *//*
     @ExcelProperty(value = "分类名",index = 5)
@@ -67,24 +101,4 @@ public class DictEeVo {
     @ApiModelProperty(value = "标签名")
     private List<String> tagNameList;*/
 
-    /**
-     * 是否置顶 (0否 1是)
-     */
-    @ExcelProperty(value = "是否置顶",index = 5)
-    @ApiModelProperty(value = "是否置顶")
-    private String isTop;
-
-    /**
-     * 是否推荐 (0否 1是)
-     */
-    @ExcelProperty(value = "文章推荐",index = 6)
-    @ApiModelProperty(value = "是否推荐 (0否 1是)")
-    private String isRecommend;
-
-    /**
-     * 状态 (1公开 2私密 3草稿)
-     */
-    @ExcelProperty(value = "文章状态",index = 7)
-    @ApiModelProperty(value = "状态 (1公开 2私密 3草稿)")
-    private String status;
 }
