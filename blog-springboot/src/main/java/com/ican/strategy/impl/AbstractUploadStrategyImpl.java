318b14703a33456d303b1d7d7feab34dd9422baa
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-10-22 16:21:02 +0800

    添加域名解析后的登录跨域 添加TinyPNG用于压缩过大上传图片

--- blog-springboot/src/main/java/com/ican/strategy/impl/AbstractUploadStrategyImpl.java
+++ blog-springboot/src/main/java/com/ican/strategy/impl/AbstractUploadStrategyImpl.java
@@ -1,13 +1,20 @@
 package com.ican.strategy.impl;
 
+import cn.hutool.core.util.ByteUtil;
+import com.ican.config.properties.TinyProperites;
 import com.ican.exception.ServiceException;
 import com.ican.strategy.UploadStrategy;
 import com.ican.utils.FileUtils;
+import com.tinify.Tinify;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * 抽象上传模板
@@ -16,10 +23,14 @@ import java.io.InputStream;
  */
 @Service
 public abstract class AbstractUploadStrategyImpl implements UploadStrategy {
+    @Autowired
+    private TinyProperites tinyProperites;
 
     @Override
     public String uploadFile(MultipartFile file, String path) {
         try {
+            //初始化Tinify，设置密钥，对大于2MB的图片进行压缩处理
+            Tinify.setKey(tinyProperites.getKey());
             // 获取文件md5值
             String md5 = FileUtils.getMd5(file.getInputStream());
             // 获取文件扩展名
@@ -28,8 +39,17 @@ public abstract class AbstractUploadStrategyImpl implements UploadStrategy {
             String fileName = md5 + "." + extName;
             // 判断文件是否已存在
             if (!exists(path + fileName)) {
-                // 不存在则继续上传
-                upload(path, fileName, file.getInputStream());
+                long fileSize = file.getSize();
+                if (fileSize < 1024L * 1024 * 1024 * 2) {
+                    upload(path, fileName, file.getInputStream());
+                } else {
+                    long strat = System.currentTimeMillis();
+                    // 使用 Tinify API 进行压缩
+                    byte[] resultData = Tinify.fromBuffer(file.getBytes()).toBuffer();
+                    System.out.println(System.currentTimeMillis() - strat / 1000 + "s");
+                    // 不存在则继续上传
+                    upload(path, fileName, new ByteArrayInputStream(resultData));
+                }
             }
             // 返回文件访问路径
             return getFileAccessUrl(path + fileName);
