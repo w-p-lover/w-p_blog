d0ef3141330de68977cd0deab4ad23fd4b6215da
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-11-14 15:51:29 +0800

    解决部署spa问题：没有指定NotFound界面跳转到index.html,因此无法根据路由查找
    使用域名指定后端管理项目URL，添加登录跨域

--- blog-springboot/src/main/java/com/ican/controller/LoginController.java
+++ blog-springboot/src/main/java/com/ican/controller/LoginController.java
@@ -36,7 +36,10 @@ public class LoginController {
      * @return {@link String} Token
      */
 
-    @CrossOrigin(value = {"http://121.41.87.40", "http://121.41.87.40:30", "http://w-love-p.top/", "http://localhost:5173", "http://localhost:5174"})
+    @CrossOrigin(value = {
+            "http://121.41.87.40", "http://121.41.87.40:30",
+            "http://w-love-p.top/","http://w-love-p.top:30/",
+            "http://localhost:5173", "http://localhost:5174"})
     @ApiOperation(value = "用户登录")
     @PostMapping("/login")
     public Result<String> login(@Validated @RequestBody LoginDTO login) {
