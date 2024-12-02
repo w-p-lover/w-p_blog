301a3ad6564d18dc0d8fe6e3786fd0d45a839572
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-12-02 13:48:46 +0800

    feat: 添加 WebSocket 配置类
    - 新增 WebSocketConfig 类，实现 WebSocketMessageBrokerConfigurer 接口
    - 配置消息代理路径
     configureMessageBroker

---
+++ blog-springboot/src/main/java/com/ican/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.ican.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // 消息订阅路径
+        config.setApplicationDestinationPrefixes("/app"); // 消息发送路径前缀
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:5173")  // 允许来自前端的连接
+                .withSockJS();  // 允许使用 SockJS 作为回退选项
+    }
+}
