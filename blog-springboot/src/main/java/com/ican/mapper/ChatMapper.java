a498fdaf5959118902410742386d1415ceab13f8
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-12-02 16:30:31 +0800

    feat(mapper): 添加 ChatMapper接口和对应的 XML 配置
    - 新增 ChatMapper 接口，继承自 BaseMapper，用于处理聊天记录相关的数据库操作
    - 添加 selectByCouple 方法，用于查询两人之间的聊天记录
    - 新增 ChatMapper.xml 文件，定义 selectByCouple 方法的 SQL 查询语句
    - SQL 查询语句使用 UNION ALL 来合并两个方向的聊天记录，并按创建时间排序

---
+++ blog-springboot/src/main/java/com/ican/mapper/ChatMapper.java
@@ -0,0 +1,22 @@
+package com.ican.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ican.entity.Chat;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Author：yep
+ * @Project：blog-springboot
+ * @name：ChatRecordMapper
+ * @Date：2024/11/25 16:24
+ * @Filename：ChatRecordMapper
+ */
+@Repository
+public interface ChatMapper extends BaseMapper<Chat> {
+
+    List<Chat> selectByCouple(@Param("send") Integer s, @Param("receive") Integer s1);
+
+}
