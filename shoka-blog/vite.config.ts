708220e4cb37eb4ce86cd7cb5fb14dffe7fe7666
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-12-02 16:44:48 +0800

    feat(shoka-blog): 添加聊天室功能并更新相关依赖
    - 在 components.d.ts 中添加了聊天室相关组件的类型定义
    - 在 package.json 中添加了 sockjs-client、@stomp/stompjs等依赖
    - 在 vite.config.ts 中添加了 global 变量定义

--- shoka-blog/vite.config.ts
+++ shoka-blog/vite.config.ts
@@ -9,6 +9,9 @@ import {createSvgIconsPlugin} from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    define: {
+        global: 'window',
+    },
     plugins: [
         vue(),
         AutoImport({
