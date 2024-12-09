11db8645e7f1dd758a1172556d36ba6f28465945
wangyoupeng2 <109458757+wangyoupeng2@users.noreply.github.com>
2024-12-09 14:02:12 +0800

    feat(FileCard): 优化文件卡片组件
    - 添加文件名和文件大小属性
    - 为文件卡片添加超链接功能
    - 优化文件名显示，添加省略号效果
    - 调整文件大小显示位置和样式

--- shoka-blog/src/components/ChatHome/Chat/FileCard.vue
+++ shoka-blog/src/components/ChatHome/Chat/FileCard.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="file-card">
-    <img src="../../../assets/img/fileImg/unknowfile.png" alt="" v-if="fileType === 0" />
-    <img src="../../../assets/img/fileImg/word.png" alt="" v-else-if="fileType === 1" />
-    <img src="../../../assets/img/fileImg/excel.png" alt="" v-else-if="fileType === 2" />
-    <img src="../../../assets/img/fileImg/ppt.png" alt="" v-else-if="fileType === 3" />
-    <img src="../../../assets/img/fileImg/pdf.png" alt="" v-else-if="fileType === 4" />
-    <img src="../../../assets/img/fileImg/zpi.png" alt="" v-else-if="fileType === 5" />
-    <img src="../../../assets/img/fileImg/txt.png" alt="" v-else />
+  <a :href="file" target="_blank" class="file-card">
+    <img src="../../../assets/img/fileImg/unknowfile.png" alt="" v-if="fileType === 0"/>
+    <img src="../../../assets/img/fileImg/word.png" alt="" v-else-if="fileType === 1"/>
+    <img src="../../../assets/img/fileImg/excel.png" alt="" v-else-if="fileType === 2"/>
+    <img src="../../../assets/img/fileImg/ppt.png" alt="" v-else-if="fileType === 3"/>
+    <img src="../../../assets/img/fileImg/pdf.png" alt="" v-else-if="fileType === 4"/>
+    <img src="../../../assets/img/fileImg/zpi.png" alt="" v-else-if="fileType === 5"/>
+    <img src="../../../assets/img/fileImg/txt.png" alt="" v-else/>
     <div class="word">
-      <span>{{ file.name || '未知' }}</span>
-      <span>154kb</span>
+      <span>{{ fileName || '未知' }}</span>
+      <span>{{ fileSize || '文件大小未知' }}</span>
     </div>
-  </div>
+  </a>
 </template>
 
 <script>
-import { defineComponent, watch, onMounted } from 'vue';
+import {defineComponent, watch, onMounted} from 'vue';
 
 export default defineComponent({
   name: 'FileCard',
@@ -26,6 +26,14 @@ export default defineComponent({
     },
     file: {
       type: Object, // File 对象类型
+      required: true,
+    },
+    fileName: {
+      type: String,
+      required: true,
+    },
+    fileSize: {
+      type: String,
       required: true,
     },
   },
@@ -61,28 +69,34 @@ export default defineComponent({
   padding: 10px;
   box-sizing: border-box;
   cursor: pointer;
+
   &:hover {
     background-color: rgb(33, 36, 54);
   }
+
   img {
     width: 60px;
     height: 60px;
   }
+
   .word {
     width: 60%;
     margin-left: 10px;
     overflow: hidden;
+
     span {
       width: 90%;
       display: inline-block;
       color: #fff;
     }
+
     span:first-child {
       font-size: 14px;
       overflow: hidden;
       white-space: nowrap;
       text-overflow: ellipsis;
     }
+
     span:last-child {
       font-size: 12px;
       color: rgb(180, 180, 180);
