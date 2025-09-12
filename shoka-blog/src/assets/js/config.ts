382294d8786c9c3b0bd813ab4e6f2bb767974e9c
wyp <3169468598@qq.com>
2025-09-12 17:04:23 +0800

    ci: 更新 OAuth 配置信息

--- shoka-blog/src/assets/js/config.ts
+++ shoka-blog/src/assets/js/config.ts
@@ -1,10 +1,10 @@
 export default {
-    // gitee的client-id
-    GITEE_APP_ID: "***************************",
-    // gitee回调地址
-    GITEE_REDIRECT_URI: "***************************",
-    // github的client-id
-    GITHUB_APP_ID: "***************************",
-    // github回调地址
-    GITHUB_REDIRECT_URL: "***************************",
+
+    GITEE_APP_ID: "0bf9c2fed0f56f2b872a9bf42feefcb81a1d46aaacc12c9774311d681a76ee34",
+
+    GITEE_REDIRECT_URI: "http://localhost:5173/oauth/login/gitee",
+
+    GITHUB_APP_ID: "Iv23li6UGS8RMCKJ5EZv",
+    // GitHub 回调地址
+    GITHUB_REDIRECT_URL: "http://localhost:5173/oauth/login/github",
 };
