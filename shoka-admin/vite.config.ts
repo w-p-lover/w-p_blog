664c7aceccbc9c4507a225686601534fd5933e1c
wyp <3169468598@qq.com>
2026-01-07 17:09:55 +0800

    杂项上传

--- shoka-admin/vite.config.ts
+++ shoka-admin/vite.config.ts
@@ -9,6 +9,14 @@ import {createSvgIconsPlugin} from "vite-plugin-svg-icons";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        target: 'es2020'
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            target: "es2020"
+        }
+    },
     plugins: [
         vue(),
         AutoImport({
